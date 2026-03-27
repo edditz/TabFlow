e0f7919e06bec56abe706d2a44b1ff68fdb6273d
Eddie <freewangei@gmail.com>
2026-03-27 22:00:52 +0800

    feat: add built-in classification rules

--- src/classification/index.ts
+++ src/classification/index.ts
@@ -2,3 +2,5 @@
 export * from './types'
 export { classifyTabs } from './classifier'
 export { getAISettings, saveAISettings, testAIConnection } from './ai-service'
+export { matchRule, CATEGORY_RULES } from './rules'
+export type { CategoryRule } from './rules'
